a38ccaa489e072770e430b9bbadd728c084bc577
MegKato <megpad3210@gmail.com>
2020-11-05 20:23:23 +0900

    banana追加

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main{
 	publlic static void main(String[] args){
 		System.out.println("Hello World");
-		System.out.println("Apple")
+		System.out.println("Apple");
+		System.out.println("Banana");
 	}
 
 }
